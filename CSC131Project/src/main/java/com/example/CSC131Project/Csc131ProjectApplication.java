a0a8ddf7b802dbb67b279c86e50cfd18f28b36ae
zli2 <zli2@csus.edu>
2022-10-05 19:20:12 -0700

    Added neccessary file for java spring

---
+++ CSC131Project/src/main/java/com/example/CSC131Project/Csc131ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CSC131Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc131ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csc131ProjectApplication.class, args);
+	}
+
+}
---
+++ CSC131Project/src/test/java/com/example/CSC131Project/Csc131ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CSC131Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc131ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
