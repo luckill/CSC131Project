da52772f2660eee38a9df620acb2d1c7c6d73dde
Anish <achouhan@csus.edu>
2022-10-09 19:22:29 -0700

    Hello World starter

--- java spring file/src/main/java/com/example/CSC131Project/Csc131ProjectApplication.java
+++ java spring file/src/main/java/com/example/CSC131Project/Csc131ProjectApplication.java
@@ -2,12 +2,24 @@ package com.example.CSC131Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Csc131ProjectApplication
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Csc131ProjectApplication.class, args);
 	}
+	@GetMapping("/hello")
+	public List<String> hello()
+	{
+		return List.of("Hello", "World");
+	}
+
 }
