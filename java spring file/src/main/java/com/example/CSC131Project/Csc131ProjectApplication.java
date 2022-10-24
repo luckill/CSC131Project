82bca61d60834c307ce0d553502f63fff5177a33
Anish <achouhan@csus.edu>
2022-10-23 18:51:15 -0800

    hello world

--- java spring file/src/main/java/com/example/CSC131Project/Csc131ProjectApplication.java
+++ java spring file/src/main/java/com/example/CSC131Project/Csc131ProjectApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
+
+
 
 @SpringBootApplication
 @RestController
@@ -14,6 +14,7 @@ public class Csc131ProjectApplication
 {
 	public static void main(String[] args)
 	{
+
 		SpringApplication.run(Csc131ProjectApplication.class, args);
 	}
 	@GetMapping("/hello")
