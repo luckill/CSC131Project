6656e8e6777b0ada47cc4f51a5ac542d9b3f33e0
Zhijun Li <zli2@csus.edu>
2022-10-29 23:22:24 -0700

    Updated AppStartUpListener.java and deleted AcademyAwardRepository.java, change the way to store academy award data from database table to hashmap

--- java spring file/src/main/java/com/example/CSC131Project/AppStartUpListener.java
+++ java spring file/src/main/java/com/example/CSC131Project/AppStartUpListener.java
@@ -1,53 +1,48 @@
 package com.example.CSC131Project;
 
+import com.example.CSC131Project.Model.*;
 import org.springframework.context.*;
 import org.springframework.context.event.*;
 import org.springframework.stereotype.*;
-
 import java.io.*;
-import java.sql.*;
 import java.util.*;
 
 @Component
 public class AppStartUpListener implements ApplicationListener<ContextRefreshedEvent>
 {
-    private AcademyAwardRepository academyAwardRepository;
+    public static Map<Integer, AcademyAward> dataMap;
 
-    public AppStartUpListener(AcademyAwardRepository academyAwardRepository)
-    {
-        this.academyAwardRepository = academyAwardRepository;
-    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event)
     {
-        if (!academyAwardRepository.findAll().iterator().hasNext())
+        int count = 0;
+        dataMap = new HashMap<>();
+        try (Scanner fileScan = new Scanner(new FileInputStream("the_oscar_award.csv")))
         {
-            try (Scanner fileScan = new Scanner(new FileInputStream("the_oscar_award.csv")))
+            fileScan.nextLine();
+            while (fileScan.hasNext())
             {
-                fileScan.nextLine();
-                while (fileScan.hasNext())
-                {
-                    String line = fileScan.nextLine();
-                    Scanner lineScanner = new Scanner(line);
-                    lineScanner.useDelimiter(",");
+                String line = fileScan.nextLine();
+                Scanner lineScanner = new Scanner(line);
+                lineScanner.useDelimiter(",");
 
-                    int yerOfFilm = Integer.parseInt(lineScanner.next());
-                    int yearOfAward = Integer.parseInt(lineScanner.next());
-                    int ceremony = Integer.parseInt(lineScanner.next());
-                    String category = lineScanner.next();
-                    String name = lineScanner.next();
-                    String film = lineScanner.next();
-                    boolean winner;
-                    winner = lineScanner.next().trim().equals("TRUE");
-                    AcademyAward award = new AcademyAward(yerOfFilm, yearOfAward, ceremony, category, name, film, winner);
-                    academyAwardRepository.save(award);
-                }
+                int yerOfFilm = Integer.parseInt(lineScanner.next());
+                int yearOfAward = Integer.parseInt(lineScanner.next());
+                int ceremony = Integer.parseInt(lineScanner.next());
+                String category = lineScanner.next();
+                String name = lineScanner.next();
+                String film = lineScanner.next();
+                boolean winner;
+                winner = lineScanner.next().trim().equals("TRUE");
+                AcademyAward award = new AcademyAward(yerOfFilm, yearOfAward, ceremony, category, name, film, winner);
+                dataMap.put(count, award);
+                count++;
             }
-            catch (FileNotFoundException e)
-            {
-                System.out.println("File not found");
-            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found");
         }
     }
 }
