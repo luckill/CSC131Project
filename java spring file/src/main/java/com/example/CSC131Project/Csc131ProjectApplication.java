79a6e346a02c0a3c4fdd0897aad42c97204e26e3
Zhijun Li <zli2@csus.edu>
2022-11-02 23:51:11 -0700

    Added the @EnableConfigurationProperties annotation - needed for ApiConfiguration

--- java spring file/src/main/java/com/example/CSC131Project/Csc131ProjectApplication.java
+++ java spring file/src/main/java/com/example/CSC131Project/Csc131ProjectApplication.java
@@ -1,18 +1,15 @@
 package com.example.CSC131Project;
 
-import com.example.CSC131Project.Controller.AcademyAwardController;
-import com.example.CSC131Project.Controller.MovieController;
-import com.example.CSC131Project.Model.Movie;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 
 
 @SpringBootApplication
-@RestController
+@EnableConfigurationProperties(ApiConfiguration.class)
 public class Csc131ProjectApplication
 {
 	public static void main(String[] args) throws JsonProcessingException {
