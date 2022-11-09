6ea943a0bda08e117c9e3080ac81dabfc3482599
christianvelapasill <114959265+christianvelapasill@users.noreply.github.com>
2022-11-08 20:29:19 -0800

    Add files via upload

--- java spring file/src/main/java/com/example/CSC131Project/Controller/MovieController.java
+++ java spring file/src/main/java/com/example/CSC131Project/Controller/MovieController.java
@@ -1,115 +1,82 @@
 package com.example.CSC131Project.Controller;
 
-import com.example.CSC131Project.ApiConfiguration;
-import com.example.CSC131Project.Model.ApiCommunicator;
+import com.example.CSC131Project.ApiCommunicator;
 import com.example.CSC131Project.Model.Movie;
 import com.example.CSC131Project.Model.MovieRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.CSC131Project.interfaceService.ImovieService;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
-@RestController
-@Component
-@Service
-@RequestMapping("/movies")
+
+
+
+
+
+
+//@ResponseBody
+
+// @RestController
+@Controller
+@RequestMapping
 public class MovieController
 {
     @Autowired
-    MovieRepository movieRepository;
-    private final ApiConfiguration apiConfiguration;
-    ApiCommunicator IMDBApi = new ApiCommunicator();
+    public ImovieService service;
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Movie> movies =service.list();
+        model.addAttribute("movies",movies);
+        return "index";
+    }
 
-    ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-    public MovieController(ApiConfiguration apiConfiguration)
-    {
-        this.apiConfiguration = apiConfiguration;
+    @GetMapping("/new")
+    public String add(Model model){
+        model.addAttribute("movie",new Movie());
+        return "AddForm";
+    }
+    @PostMapping("/save")
+    public String save(@Valid Movie m, Model model){
+        service.save(m);
+        return "redirect:/list";
     }
 
 
+    @Autowired
+    public MovieRepository movieRepository;
     @GetMapping("/getMovieById/{movieId}")
     public String getMovieById(@PathVariable String movieId)
     {
         //placeholder, replace with your code
         return "";
     }
-    //Finds movie as a title first looks through the db
-    //if nothing found in db goes to the IMDBApi and adds it to the db and returns it
     @GetMapping("/getMovieByTitle/{title}")
-    public String getMovieByTitle(@PathVariable String title) throws JsonProcessingException {
-        List<Movie> movies = movieRepository.findByTitle(title);
-        if(movies.size() != 0){
-            return objectMapper.writeValueAsString(movies.get(0));
-        }
-        String json = IMDBApi.getRequest(title, apiConfiguration.AuthorizeToke());
-        if(!json.contains("Movie not found!"))
-        {
-            Movie thisMovie = objectMapper.readValue(json, Movie.class);
-            thisMovie = movieRepository.save(thisMovie);
-            return objectMapper.writeValueAsString(thisMovie);
-        }
-        return objectMapper.writeValueAsString(objectMapper.readTree(json));
+    public String getMovieByTitle(@PathVariable String title)
+    {
+        return "";
     }
     @PostMapping("/addMovie/{title}")
     public String addMovie(@PathVariable String title) throws ParseException
     {
         return "";
     }
-    @PutMapping("/updateMovie/{title}")
-    public String updateMovie(@RequestBody String body, @PathVariable String title) throws JsonProcessingException
+    @PutMapping("")
+    public String updateMovie()
     {
-        Movie movie = movieRepository.find1Movie(title);
-        if(movie != null && body != null) //Needs better error checking on an empty send, e.g. no {} post.
-        {
-            Movie temp = objectMapper.readValue(body, Movie.class);
-
-            if(temp.getTitle()!= null)  //Each if checks for a user inputted put/edit. And sets if it exists.
-            {
-                movie.setTitle(temp.getTitle());
-            }
-            if(temp.getDirector() != null)
-            {
-                movie.setDirector(temp.getDirector());
-            }
-            if(temp.getYear() != 0)
-            {
-                movie.setYear(temp.getYear());
-            }
-            if (temp.getLanguage() != null)
-            {
-                movie.setLanguage(temp.getLanguage());
-            }
-            movieRepository.save(movie);
-            return objectMapper.writeValueAsString(movie);
-        }
-        else
-        {
-            return "Movie not found";
-        }
-
-    } //update movie closing
-
-
-    @Modifying
-    @DeleteMapping("/delete/{ID}")
-    public String deleteMovieById(@PathVariable int ID) throws JsonProcessingException {
-        List<Movie> movies = movieRepository.findByID(ID);
-        if(movies.size() != 0){
-            movieRepository.deleteByID(ID);
-            return objectMapper.writeValueAsString(movies.get(0));
-        }
-        String error = "{\"Error\": \"Movie id does not exist\"}";
-        return objectMapper.writeValueAsString(objectMapper.readTree(error));
+        return "";
+    }
+    @DeleteMapping("/delete/{movieId}")
+    public String deleteMovieById(@PathVariable String movieId)
+    {
+        return"";
     }
 
 }
