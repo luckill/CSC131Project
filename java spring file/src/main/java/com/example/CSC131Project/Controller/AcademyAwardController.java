ec36ed97a2a73f890bc9600f8148a4b6241a78a3
Zhijun Li <zli2@csus.edu>
2022-11-19 23:23:29 -0800

    Updated AcademyAward.java, finsh the winner and nominee query as well as updated the retrun result to show both the movie and the academy award data

--- java spring file/src/main/java/com/example/CSC131Project/Controller/AcademyAwardController.java
+++ java spring file/src/main/java/com/example/CSC131Project/Controller/AcademyAwardController.java
@@ -5,10 +5,7 @@ import com.example.CSC131Project.Model.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.*;
 
 @RestController
@@ -29,8 +26,6 @@ public class AcademyAwardController
     @GetMapping("/actor/{year}")
     public String FindActorAwardByYear(@PathVariable("year") int year)
     {
-        System.out.println(year);
-        System.out.println();
         String result = "";
         try
         {
@@ -40,13 +35,15 @@ public class AcademyAwardController
                     filter(academyAward -> academyAward.getYearOfAward() == year && academyAward.isWinner() && academyAward.getCategory().contains("ACTOR")).
                     toList();
             JSONArray array = new JSONArray();
+
             for (AcademyAward s : awardList)
             {
                 String title = s.getFilm();
                 JSONObject obj = new JSONObject();
+                JSONObject movieObj = new JSONObject();
                 obj.put("Award", s.getCategory());
                 obj.put("Year", s.getYearOfAward());
-                obj.put("file tile", s.getFilm());
+                obj.put("film tile", s.getFilm());
                 array.add(obj);
                 if (movieRepository.findByTitle(title).isEmpty())
                 {
@@ -60,10 +57,28 @@ public class AcademyAwardController
                     String language = (String) json.get("Language");
                     String movieID = (String) json.get("imdbID");
                     int yearOfFilm = Integer.parseInt((String)json.get("Year"));
+                    movieObj.put("title", filmTitle);
+                    movieObj.put("Director", director);
+                    movieObj.put("Language", language);
+                    movieObj.put("imdbID", movieID);
+                    movieObj.put("Year", yearOfFilm);
 
                     Movie movie = new Movie(movieID, filmTitle, director, yearOfFilm, language);
                     movieRepository.save(movie);
                 }
+                else
+                {
+                    List<Movie> movieList = movieRepository.findByTitle(title);
+                    for(Movie movie: movieList)
+                    {
+                        movieObj.put("title", movie.getTitle());
+                        movieObj.put("Director", movie.getDirector());
+                        movieObj.put("Language", movie.getLanguage());
+                        movieObj.put("imdbID", movie.getMovieID());
+                        movieObj.put("Year", movie.getYear());
+                    }
+                }
+                array.add(movieObj);
             }
             result = array.toJSONString();
         }
@@ -72,6 +87,34 @@ public class AcademyAwardController
             ex.printStackTrace();
         }
         return result;
+    }
+
+    @GetMapping("AwardWinnerAndNominee/{year}")
+    public String getAwardWinnerAndNomineeByYear(@PathVariable("year") int year)
+    {
+        List<AcademyAward> awardList = AppStartUpListener.dataMap.values().stream().filter(academyAward -> academyAward.getYearOfAward() == year).toList();
+        JSONArray array = new JSONArray();
+        JSONArray nominee = new JSONArray();
+        JSONArray winner = new JSONArray();
+        for (AcademyAward s: awardList)
+        {
+            JSONObject obj = new JSONObject();
+            obj.put("Award", s.getCategory());
+            obj.put("Year", s.getYearOfAward());
+            obj.put("film tile", s.getFilm());
+
+            if (s.isWinner())
+            {
+                winner.add(obj);
+            }
+            else
+            {
+                nominee.add(obj);
+            }
+        }
+        array.add(winner);
+        array.add(nominee);
+        return array.toJSONString();
     }
 }
 
